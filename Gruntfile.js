dee76eed602ab458fdeb644d619164003169f559
Yavor Ivanov <yavor.ivanov@sap.com>
2019-04-24 13:57:36 +0300

    Frontend App template

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function (grunt) {
+	"use strict";
+	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
+	grunt.config.merge({ compatVersion: "edge" });
+	grunt.registerTask("default", [
+		"clean",
+		"lint",
+		"build"
+	]);
+	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-test");
+	grunt.registerTask("unit_and_integration_tests", ["test"]);
+	grunt.config.merge({
+		coverage_threshold: {
+			statements: 0,
+			branches: 100,
+			functions: 0,
+			lines: 0
+		}
+	});
+};
---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("ui5con.ui5con.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
