976cfe238a734c842c9a767e577b9905b65e49c7
Yavor Ivanov <yavor.ivanov@sap.com>
2019-05-09 14:49:46 +0300

    GraphQL enablement

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+// Create the User Schema.
+const UserSchema = new Schema({
+	id: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	name: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	}
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
