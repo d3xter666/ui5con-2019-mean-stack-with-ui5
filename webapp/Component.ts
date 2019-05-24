ab3ac4a4b975b5e6fa63f52d31a2846d9a1564f3
Yavor Ivanov <yavor.ivanov@sap.com>
2019-05-24 11:04:37 +0300

    Add TypeScript linting
    - Add TSLint
    - Fix TSLint errors

--- webapp/Component.ts
+++ webapp/Component.ts
@@ -1,30 +1,31 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent",
-	"sap/ui/Device",
-	"ui5con2019/model/models"
-], function (UIComponent, Device, models) {
-	"use strict";
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "ui5con2019/model/models",
+], (UIComponent, Device, models) => {
+    "use strict";
 
-	return UIComponent.extend("ui5con2019.Component", {
+    return UIComponent.extend("ui5con2019.Component", {
 
-		metadata: {
-			manifest: "json"
-		},
+        metadata: {
+            manifest: "json",
+        },
 
-		/**
-		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-		 * @public
-		 * @override
-		 */
-		init: function () {
-			// call the base component's init function
-			UIComponent.prototype.init.apply(this, arguments);
+        /**
+         * The component is initialized by UI5 automatically during the
+         * startup of the app and calls the init method once.
+         * @public
+         * @override
+         */
+        init() {
+            // call the base component's init function
+            UIComponent.prototype.init.apply(this, arguments);
 
-			// enable routing
-			this.getRouter().initialize();
+            // enable routing
+            this.getRouter().initialize();
 
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
-		}
-	});
+            // set the device model
+            this.setModel(models.createDeviceModel(), "device");
+        },
+    });
 });
--- webapp/controller/View1.controller.ts
+++ webapp/controller/View1.controller.ts
@@ -1,18 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-], function (
-    Controller,
-    JSONModel
-) {
+], (Controller, JSONModel) => {
     "use strict";
 
 
     return Controller.extend("ui5con2019.controller.BaseController", {
-        onChange: function () {
-            var oModel = this.getView().getModel("zzz");
+        onChange() {
+            const oModel = this.getView().getModel("zzz");
 
             oModel.loadData("/api/v2/zzz", oModel.getData(), true, "POST");
-        }
+        },
     });
 });
