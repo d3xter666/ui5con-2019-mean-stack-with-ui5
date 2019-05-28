30564474ba47fa0f538ccabbf199206aad14c73a
Yavor Ivanov <yavor.ivanov@sap.com>
2019-05-28 17:40:16 +0300

    Frontend: Graphql model

--- webapp/libs/ui5con/model/graphql/GraphQLModel.ts
+++ webapp/libs/ui5con/model/graphql/GraphQLModel.ts
@@ -1,7 +1,45 @@
-sap.ui.define(["sap/ui/model/json/JSONModel"], (JSONModel) => {
+sap.ui.define(["sap/ui/model/Model"], (Model) => {
     "use strict";
 
-    const GraphQLModel = JSONModel.extend("ui5con2019.ui5con2019.model.graphql.GraphQLModel", {});
+    const GraphQLModel = Model.extend("ui5con.model.graphql.GraphQLModel", {});
+
+    GraphQLModel.prototype.query = function(url: string, query: string): Promise<any> {
+        return fetch(url, {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({query}),
+        })
+            .then((data: any) => {
+                this.oData = data;
+
+                this.fireRequestCompleted();
+
+                return data;
+            }, (err) => {
+                this.fireRequestFailed(err);
+
+                throw new Error(err);
+            });
+    };
+
+    GraphQLModel.prototype.getData = function(): any {
+        return this.oData;
+    };
+
+    // @ts-ignore
+    GraphQLModel.prototype.setData = function(data: any): ui5con.model.graphql.GraphQLModel {
+        this.oData = data;
+
+        return this;
+    };
+
+    // tslint:disable-next-line:no-unused-expression
+    new GraphQLModel();
+
 
     return GraphQLModel;
 });
