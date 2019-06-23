6bad04fec371dd836667ec15c0fbf0f52ef20036
Yavor Ivanov <yavor.ivanov@sap.com>
2019-06-23 13:07:57 +0300

    - Integrate GraphQL with MongoDB - Include data importer, so DB won't be empty

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,28 @@
 (function (global, module) {
 	const express = require("express");
 	const expressGraphql = require("express-graphql");
+	const mongoose = require("mongoose");
 	const bodyParser = require("body-parser");
 	const cors = require("cors");
-	const schema = require("./graphql/index");
+	const schema = require("./graphql/");
+	const dataImporter = require("./mock-data/import-data");
 
 	const app = express();
 	const PORT = process.env.PORT || "8080";
+	const db = process.env.MONGO || "mongodb://localhost:27017/local";
+
+	// Connect to MongoDB with Mongoose.
+	mongoose
+		.connect(
+			db,
+			{
+				useCreateIndex: true,
+				useNewUrlParser: true
+			}
+		)
+		.then(dataImporter)
+		.then(() => console.log("Mongo collection loaded"))
+		.catch(err => console.log(err));
 
 	app.use(
 		"/graphql",
