c15b8d33f5f05e50d6e257680f6cf81cadd59602
Yavor Ivanov <yavor.ivanov@sap.com>
2019-06-23 11:24:58 +0300

    Rename files & switch to typescript

---
+++ webapp/Component.ts
@@ -0,0 +1,37 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "ui5con2019/model/models",
+], (UIComponent: sap.ui.core.UIComponent,
+    // @ts-ignore
+    Device: sap.ui.Device,
+    // @ts-ignore
+    models: ui5con2019.model.models) => {
+    "use strict";
+
+    // @ts-ignore
+    return UIComponent.extend("ui5con2019.Component", {
+
+        metadata: {
+            manifest: "json",
+        },
+
+        /**
+         * The component is initialized by UI5 automatically during the
+         * startup of the app and calls the init method once.
+         * @public
+         * @override
+         */
+        init() {
+            // call the base component's init function
+            // @ts-ignore
+            UIComponent.prototype.init.apply(this, arguments);
+
+            // enable routing
+            this.getRouter().initialize();
+
+            // set the device model
+            this.setModel(models.createDeviceModel(), "device");
+        },
+    });
+});
---
+++ webapp/controller/View1.controller.ts
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+], (Controller: sap.ui.core.mvc.Controller) => {
+
+    // @ts-ignore
+    return Controller.extend("ui5con2019.controller.BaseController", {
+        onInit(): void {
+            // TODO: Init hook
+        },
+    });
+});
