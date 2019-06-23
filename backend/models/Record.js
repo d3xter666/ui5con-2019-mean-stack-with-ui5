98663dff76c4d966e8ba2dd825c740bdad072985
Yavor Ivanov <yavor.ivanov@sap.com>
2019-06-23 13:07:57 +0300

    - Integrate GraphQL with MongoDB - Include data importer, so DB won't be empty

---
+++ backend/models/Record.js
@@ -0,0 +1,54 @@
+(function (global, module) {
+	const mongoose = require("mongoose");
+
+	const Schema = mongoose.Schema;
+
+	// Create the Record Schema.
+	const RecordSchema = new Schema({
+		id: {
+			type: Number,
+			required: true,
+			unique: true
+		},
+		avatar: {
+			type: String
+		},
+		title: {
+			type: String
+		},
+		first_name: {
+			type: String
+		},
+		last_name: {
+			type: String
+		},
+		job_title: {
+			type: String
+		},
+		email: {
+			type: String
+		},
+		company: {
+			type: String
+		},
+		department: {
+			type: String
+		},
+		languages: {
+			type: [String]
+		},
+		university: {
+			type: String
+		},
+		self_decription: {
+			type: String
+		},
+		skills: {
+			type: [String]
+		}
+	});
+
+	const Record = mongoose.model("Record", RecordSchema);
+
+	module.exports = Record;
+}(this, module));
