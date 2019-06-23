df69e1997958b2befe73d1386a0fb8ae4eee3fd1
Yavor Ivanov <yavor.ivanov@sap.com>
2019-06-23 16:21:07 +0300

    Bugfixes

--- webapp/model/models.ts
+++ webapp/model/models.ts
@@ -1,16 +1,17 @@
 sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "sap/ui/Device",
-], (JSONModel, Device) => {
+], (JSONModel: sap.ui.model.json.JSONModel,
+    // @ts-ignore
+    Device: sap.ui.Device) => {
     "use strict";
 
     return {
-
-        createDeviceModel() {
+        createDeviceModel(): sap.ui.model.json.JSONModel {
+            // @ts-ignore
             const oModel = new JSONModel(Device);
             oModel.setDefaultBindingMode("OneWay");
             return oModel;
         },
-
     };
 });
